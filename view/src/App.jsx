8d1f7943dd85126158dc71f2103cbf5db0d21551
sukrijakararic <sukrijakararic1@gmail.com>
2024-10-25 22:35:07 +0200

    started o implement more features in landing page

--- view/src/App.jsx
+++ view/src/App.jsx
@@ -8,9 +8,12 @@ import {
 } from "react-router-dom";
 
 import { Root } from "./components/Root/Root";
+import { LandingPagePic } from "./components/LandingPagePic/LandingPagePic";
 
 const appRouter = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<Root />}></Route>)
+  createRoutesFromElements(<Route path="/" element={<Root />}>
+    <Route path="index"  element={<LandingPagePic />}/>
+  </Route>)
 );
 
 function App() {
---
+++ view/src/components/LandingPagePic/LandingPagePic.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './LandingPagePic.module.css'
+
+export const LandingPagePic = () => {
+  return (
+    <div className={styles.LandingPic}>
+        <img src="/src/assets/fleet_4472x.webp" alt="" />
+    </div>
+  )
+}
