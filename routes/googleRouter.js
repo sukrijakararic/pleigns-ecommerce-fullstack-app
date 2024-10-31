47fe0372baa64ab9082c7b301a249fb8c407c3a0
Sukrija Kararic <116309642+sukrijakararic@users.noreply.github.com>
2024-10-31 13:51:58 +0100

    changed the server to redirect the user to profile if logged in through google. Also added query to stop user from adding self twice to db

--- routes/googleRouter.js
+++ routes/googleRouter.js
@@ -17,12 +17,20 @@ module.exports = (app) => {
     }),
     async function (req, res) {
       try {
+        const result = await db.query(
+          "SELECT * FROM users WHERE google_profile ->> 'sub' = $1",
+          [req.user.sub]
+        );
+        if (result.rows.length > 0) {
+          return res.status(200).redirect("http://localhost:5173/profile");
+        }
+
         await db.query(
           "INSERT INTO users (google_profile) VALUES ($1) RETURNING *",
           [req.user]
         );
 
-        res.redirect("/");
+        res.status(200).redirect("http://localhost:5173/profile");
 
       } catch (err) {
         console.log(err);
