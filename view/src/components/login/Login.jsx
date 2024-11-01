6603ff82ceffc07fb45662b879323c81992f9b66
Sukrija Kararic <116309642+sukrijakararic@users.noreply.github.com>
2024-11-01 20:24:00 +0100

    created logic for login component to work with controller

--- view/src/components/login/Login.jsx
+++ view/src/components/login/Login.jsx
@@ -1,21 +1,44 @@
 import React from "react";
 import styles from "./Login.module.css";
-import Form from 'react-bootstrap/Form';
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from "react-bootstrap/Form";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Button from "react-bootstrap/Button";
+import { AuthContext } from "../../utils/AuthContext";
+import { useContext } from "react";
+
+import { login } from "../../utils/utils";
 
 export const Login = () => {
+  const { setLoggedIn } = useContext(AuthContext);
+
+const handleLogin = async (event) => {
+  event.preventDefault();
+  const data = new FormData(event.target);
+  const user = {
+    email: data.get("email"),
+    password: data.get("password"),
+  };
+  const response = await login(user);
+  event.target.reset();
+  document.getElementById("responseStatus").textContent = response.message;
+  response.message.includes("Logged in") && setLoggedIn(true);
+};
   return (
-    <form>
+    <form onSubmit={handleLogin}>
       <FloatingLabel
         controlId="floatingInput"
         label="Email address"
         className="mb-3"
       >
-        <Form.Control type="email" placeholder="name@example.com" />
+        <Form.Control type="email" name="email" placeholder="name@example.com" />
       </FloatingLabel>
       <FloatingLabel controlId="floatingPassword" label="Password">
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control type="password" name="password" placeholder="Password" />
       </FloatingLabel>
+      <p className={styles.responseStatus} id="responseStatus"></p>
+      <Button variant="success" type="submit">
+        Submit
+      </Button>
     </form>
   );
 };
