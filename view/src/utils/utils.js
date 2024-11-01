143ed28ad596a4ee295030e3cadf74f6796ef810
Sukrija Kararic <116309642+sukrijakararic@users.noreply.github.com>
2024-11-01 19:39:31 +0100

    added login func to my utils

--- view/src/utils/utils.js
+++ view/src/utils/utils.js
@@ -1,33 +1,48 @@
-
 export const addUser = async (user) => {
-    try {
-      const response = await fetch('http://localhost:4000/users/register', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(user),
-      });
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error('Error adding user:', error);
-      throw error;
-    }
-  };
+  try {
+    const response = await fetch("http://localhost:4000/users/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error adding user:", error);
+    throw error;
+  }
+};
 
-  export const logout = async () => {
-    try {
-      const response = await fetch('http://localhost:4000/users/logout', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error('Error logging out:', error);
-      throw error;
-    }
-  };
+export const login = async (user) => {
+  try {
+    const response = await fetch("http://localhost:4000/users/login", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const logout = async () => {
+  try {
+    const response = await fetch("http://localhost:4000/users/logout", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error logging out:", error);
+    throw error;
+  }
+};
