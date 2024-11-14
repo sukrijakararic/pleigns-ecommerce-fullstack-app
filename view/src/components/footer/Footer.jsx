16d431a97c63df1b7925dde103044913a4280c11
Sukrija Kararic <116309642+sukrijakararic@users.noreply.github.com>
2024-11-14 20:34:43 +0100

    changed footer to be a div to fix visual bug.

--- view/src/components/footer/Footer.jsx
+++ view/src/components/footer/Footer.jsx
@@ -1,22 +1,11 @@
 import React from "react";
 import styles from "./Footer.module.css";
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
 
 
 export const Footer = () => {
   return (
-      <Navbar className="bg-body-tertiary" fixed="bottom" >
-        <Container>
-          <Navbar.Brand>Pleigns</Navbar.Brand>
-          <Navbar.Toggle />
-          <Navbar.Collapse className="justify-content-end">
-            <Navbar.Text>
-              Made with &lt;3 frustration &lt;3
-            </Navbar.Text>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    
+    <div className={styles.footer}>
+      <p>Made with &lt;3 frustration &lt;3</p>
+    </div>
   );
 };
