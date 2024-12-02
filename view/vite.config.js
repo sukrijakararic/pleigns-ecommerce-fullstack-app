5cc901b002ce7d44ff408ef0a3436beead4d7274
sukrijakararic <sukrijakararic1@gmail.com>
2024-12-03 00:45:25 +0100

    changed proxy

--- view/vite.config.js
+++ view/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: 'https://pleigns-api.onrender.com',
         changeOrigin: true,
       },
     },
