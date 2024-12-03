86c03fce6594d3ddcd91808da092536f094cd717
Sukrija Kararic <116309642+sukrijakararic@users.noreply.github.com>
2024-12-03 15:13:25 +0100

    changed config and rebuild

--- view/vite.config.js
+++ view/vite.config.js
@@ -5,12 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'htttp://localhost:4000',
-        changeOrigin: true,
-      },
-    },
-  },
+  
 });
