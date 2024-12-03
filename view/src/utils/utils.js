9ef6842096917839b27223da364668638e20e710
Sukrija Kararic <116309642+sukrijakararic@users.noreply.github.com>
2024-12-03 14:25:37 +0100

    changed path

--- view/src/utils/utils.js
+++ view/src/utils/utils.js
@@ -91,7 +91,7 @@ export const addToCart = async (productId, qty) => {
 
 export const getCart = async () => {
   try {
-    const response = await fetch("https://pleigns-api.onrender.com/api/cart/myCart");
+    const response = await fetch("/api/cart/myCart");
     const data = await response.json();
     return data;
   } catch (error) {
