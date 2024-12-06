dd86bdbf7f93943c8b246cc7b0710bf150e0a329
Sukrija Kararic <116309642+sukrijakararic@users.noreply.github.com>
2024-12-06 13:39:39 +0100

    reverted change

--- strategies/googleStrategy.js
+++ strategies/googleStrategy.js
@@ -8,7 +8,7 @@ module.exports = function (passport) {
         clientID:
           CLIENTID,
         clientSecret: CLIENTSECRET,
-        callbackURL: '/api/google/callback',
+        callbackURL: 'https://pleigns-api.onrender.com/api/google/callback',
         passReqToCallback: true,
       },
       function (request, accessToken, refreshToken, profile, done) {
