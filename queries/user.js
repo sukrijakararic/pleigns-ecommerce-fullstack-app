ea0db9ad8826d9efdc16371690b590fb99c5f051
Sukrija Kararic <116309642+sukrijakararic@users.noreply.github.com>
2024-12-06 19:46:10 +0100

    code change testQ

--- queries/user.js
+++ queries/user.js
@@ -142,9 +142,6 @@ const getUserByIdForRouter = async (request, response, next) => {
 
 const showUser = async (request, response, next) => {
   console.log("showUser: ", request.user);
-  if (!request.user) {
-    response.status(401).json({ message: "Please log in" });
-  } else {
     const result = await db.query("SELECT email, firstname, lastname FROM users where email = $1", [
       request.user.email,
     ]);
