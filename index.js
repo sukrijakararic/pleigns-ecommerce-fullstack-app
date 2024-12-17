2f33fd364755790bab92878ca746a9dd5f671501
sukrijakararic <sukrijakararic1@gmail.com>
2024-12-17 23:19:04 +0100

    reverted fetch and removed cookie

--- index.js
+++ index.js
@@ -39,11 +39,6 @@ app.use(
     secret: SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    cookie: {
-      maxAge: 60 * 60 * 1000,
-      httpOnly: true,
-      secure: true,
-    },
   })
 );
 
