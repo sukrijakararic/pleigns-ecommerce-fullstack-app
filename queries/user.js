a3f8e405dd24ee5af426defdba6b937e90f19b81
sukrijakararic <sukrijakararic1@gmail.com>
2024-12-17 22:53:05 +0100

    added a function check for test

--- queries/user.js
+++ queries/user.js
@@ -142,7 +142,7 @@ const getUserByIdForRouter = async (request, response, next) => {
 
 const showUser = async (request, response, next) => {
   console.log("showUser: ", request.user);
-  if (!request.user) {
+  if (!request.isAuthenticated()) {
     response.status(401).json({ message: "Please log in" });
   } else {
     const result = await db.query("SELECT email, firstname, lastname FROM users where email = $1", [
