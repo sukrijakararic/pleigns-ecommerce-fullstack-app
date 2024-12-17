756405935255d93301bbfd832a8ab320de87308d
sukrijakararic <sukrijakararic1@gmail.com>
2024-12-17 23:04:56 +0100

    configured cors

--- index.js
+++ index.js
@@ -18,7 +18,12 @@ const googleRouter = require("./routes/googleRouter");
 const githubRouter = require("./routes/githubRouter");
 
 // security
-app.use(cors());
+app.use(cors({
+  origin: "https://pleigns-ecommerce-fullstack-app.onrender.com",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Access-Control-Allow-Origin"],
+}));
 app.use(helmet());
 
 
