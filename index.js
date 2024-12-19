894a3ea266cbde32babe1df9fe7be2502a35a4c2
Sukrija Kararic <116309642+sukrijakararic@users.noreply.github.com>
2024-12-19 20:23:59 +0100

    enable RedisStore usage in session configuration

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ redisClient.connect().catch(console.error);
 
 app.use(
   session({
-    //store: new RedisStore({ client: redisClient }),
+    store: new RedisStore({ client: redisClient }),
     secret: SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
